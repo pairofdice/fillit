c1420178b0a6f0b62715df980bdf81c67d1073b6
Llayaz <llaiyaz@gmail.com>
2022-03-02 10:56:42 +0200

    fixed the too many lines problem in parse_piece

--- src/parse.c
+++ src/parse.c
@@ -14,7 +14,7 @@
 #include "fillit.h"
 #include <stdio.h>
 
-void	ft_delete_tetri(t_piece **tetri_set, int i)
+int	ft_delete_tetri(t_piece **tetri_set, int i)
 {
 	if (i > 0)
 	{
@@ -24,6 +24,7 @@ void	ft_delete_tetri(t_piece **tetri_set, int i)
 			*tetri_set = NULL;
 		}
 	}
+	return (1);
 }
 
 void	parse_piece(char *input, t_piece *piece)
@@ -53,25 +54,20 @@ int	ft_parse(char *input, t_piece **tetri_set, int tetri_nb)
 	size_t	len;
 
 	len = ft_strlen(input);
-	i = 0;
-	while (i < tetri_nb + 1)
+	i = -1;
+	while (++i < tetri_nb + 1)
 	{
 		tetri_set[i] = malloc(sizeof(t_piece));
 		if (!tetri_set[i])
+			return (ft_delete_tetri(tetri_set, i));
+		if (i < tetri_nb)
 		{
-			ft_delete_tetri(tetri_set, i);
-			return (1);
+			tetri_set[i]->name = (char)('A' + i);
+			parse_piece(input, tetri_set[i]);
+			input = input + 21;
 		}
-		i++;
 	}
-	i = 0;
-	while (i < tetri_nb)
-	{
-		tetri_set[i]->name = (char)('A' + i);
-		parse_piece(input, tetri_set[i]);
-		input = input + 21;
-		i++;
-	}
+	i--;
 	tetri_set[i]->name = ('x');
 	j = -1;
 	while (j++ < 5)
